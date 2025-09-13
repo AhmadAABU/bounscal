7f63096c14ea91edee22ded442fd4f5bf27cb21c
Ahmad Jawdat <Ahmad.jawdat@mail.ru>
2025-09-14 01:46:23 +0300

    edit media query

--- src/CommissionCalculator.jsx
+++ src/CommissionCalculator.jsx
@@ -97,7 +97,7 @@ export default function CommissionCalculator({ isAccountant }) {
   const finalSalary = Number(monthlyIncome) + streamerBonus;
 
   return (
-    <div className="bg-[#f3f3f3] flex h-screen justify-center items-center px-4 md:px-0 dark:bg-[#081020]">
+    <div className="bg-[#f3f3f3] flex h-screen justify-center items-center px-4 md:px-0 dark:bg-[#081020] overflow-y-auto">
       <div className="w-full max-w-3xl mx-auto p-4 sm:p-6 bg-white rounded-xl shadow-md relative dark:bg-gray-800 dark:text-gray-100 ">
         <div className="absolute bottom-0 right-0 pr-3 pb-3">
           <button
